887923ef54c299fb3ce7c2b63e185e8a1d4b0cf2
JosheFlai1308 <jose.chavezr@sansano.usm.cl>
2024-03-29 14:59:59 -0300

    creacion proyecto

---
+++ proyecto-ingco/src/main/java/com/proyecto/software/proyectoingco/ProyectoIngcoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.software.proyectoingco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIngcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIngcoApplication.class, args);
+	}
+
+}
---
+++ proyecto-ingco/src/test/java/com/proyecto/software/proyectoingco/ProyectoIngcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.software.proyectoingco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIngcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
