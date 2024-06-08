d08cee4df3464ebd9a84888dc2362fd6698fb25e
JosheFlai1308 <jose.chavezr@sansano.usm.cl>
2024-06-08 02:43:47 -0400

    creacion de index para encargado

--- backend/src/main/java/com/grupo2/backend/dto/EncargadoDto.java
+++ backend/src/main/java/com/grupo2/backend/dto/EncargadoDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EncargadoDto {
-        private int id;
+    private int id;
 
     private String nombreEncargado;
 
