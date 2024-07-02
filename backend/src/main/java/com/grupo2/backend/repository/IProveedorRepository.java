1ccf49fe6030e2bebf0807eaf27df3e1f33894e5
SekiDesu0 <vichoaranguiz04@gmail.com>
2024-07-02 05:43:13 -0400

    cleanups and fixes :3

--- backend/src/main/java/com/grupo2/backend/repository/IProveedorRepository.java
+++ backend/src/main/java/com/grupo2/backend/repository/IProveedorRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.grupo2.backend.entity.ProveedorEntity;
@@ -15,13 +14,12 @@ public interface IProveedorRepository extends CrudRepository<ProveedorEntity, In
     @Query("SELECT p FROM ProveedorEntity p WHERE CONCAT(p.nombre_proveedor, p.categoria) LIKE %?1%")
     public List<ProveedorEntity> findAll(String search);
 
-
     @Query("SELECT pe.nota_conformidad FROM PedidoEntity pe " +
-           "JOIN pe.linea_detalle_p ldp " +
-           "JOIN ldp.producto prod " +
-           "JOIN prod.prod_prov pp " +
-           "JOIN pp.proveedor prov " +
-           "WHERE prov.id = ?1")
+            "JOIN pe.linea_detalle_p ldp " +
+            "JOIN ldp.producto prod " +
+            "JOIN prod.prod_prov pp " +
+            "JOIN pp.proveedor prov " +
+            "WHERE prov.id = ?1")
     public List<Float> getNotasConformidadPedido(int id_proveedor);
 
 }
